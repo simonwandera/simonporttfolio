86a7c779afbe8e28db26c599e2e410c628ebb6a5
simonwandera <simonwandera12@gmail.com>
2022-06-22 16:41:25 +0300

    Portfolio commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from "react-dom/client";
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+import App from './App';
+import ThemeContextProvider from './contexts/ThemeContext'
+
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <ThemeContextProvider>
+  <App />
+  </ThemeContextProvider>,
+);
+
+reportWebVitals();
